b46e56cfac29774f907efa602452270ce264f5b2
Henrik Singmann <singmann@gmail.com>
2020-02-20 10:59:23 +0000

    ignore development.R

--- development.R
+++ development.R
@@ -1,8 +1,12 @@
 
 
 ### basic package things
+usethis::use_build_ignore("development.R")
+
 library(usethis)
 
 # Create a new package -------------------------------------------------
 create_package("stanova")
 use_git()
+
+
