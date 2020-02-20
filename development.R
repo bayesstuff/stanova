53659f2ff8f5228ff40e12602a92154f94c93e58
Henrik Singmann <singmann@gmail.com>
2020-02-20 16:41:07 +0000

    other stuff

--- development.R
+++ development.R
@@ -1,7 +1,16 @@
+library("devtools")
+load_all()
 
+document()
 
 ### basic package things
 usethis::use_build_ignore("development.R")
+usethis::use_build_ignore("examples/")
+
+use_package("rstanarm")
+use_package("lme4")
+use_package("emmeans")
+use_package("rstan")
 
 library(usethis)
 
@@ -9,4 +18,4 @@ library(usethis)
 create_package("stanova")
 use_git()
 
-
+use_roxygen_md()
