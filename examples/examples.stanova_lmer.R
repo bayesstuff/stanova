f0e4d97656cdca8623e8915d8f80f5eb093c64cd
Henrik Singmann <singmann@gmail.com>
2020-02-20 16:59:21 +0000

    added second example

--- examples/examples.stanova_lmer.R
+++ examples/examples.stanova_lmer.R
@@ -9,3 +9,10 @@ str(out_list)
 
 out_list
 
+data("obk.long", package = "afex")
+
+m2 <- stanova_lmer(value ~ treatment * phase + (1|id), obk.long,
+                   chains = 2, iter = 500)
+
+m2
+summary(m2)
