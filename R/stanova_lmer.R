2b0cbe89464e8c63da61170da947d5e4739dd57f
Henrik Singmann <singmann@gmail.com>
2020-02-21 13:01:50 +0000

    addes stanova_glmer

--- R/stanova_lmer.R
+++ R/stanova_lmer.R
@@ -22,15 +22,38 @@ stanova_lmer <- function(
     data <- check_contrasts(formula = formula, data = data,
                             new_contrast = check_contrasts)
   }
-   mout <- rstanarm::stan_glmer(
-     formula = formula,
-     data = data,
-     family = "gaussian",
-     ...)
-   mout$call <- call
-   mout$stan_function <- "stanova_lmer"
-   class(mout) <- c("stanova", class(mout))
-   return(mout)
+  mout <- rstanarm::stan_glmer(
+    formula = formula,
+    data = data,
+    family = "gaussian",
+    ...)
+  mout$call <- call
+  mout$stan_function <- "stanova_lmer"
+  class(mout) <- c("stanova", class(mout))
+  return(mout)
+}
+
+#' @export
+stanova_glmer <- function(
+  formula,
+  data,
+  family,
+  check_contrasts = "contr.bayes",
+  ...) {
+  call <- match.call(expand.dots = TRUE)
+
+  if (!is.null(check_contrasts)) {
+    data <- check_contrasts(formula = formula, data = data,
+                            new_contrast = check_contrasts)
+  }
+  call["check_contrasts"] <- NULL
+
+  call[[1]] <- rstanarm::stan_glmer
+  mout <- eval(call)
+  mout$call <- call
+  mout$stan_function <- "stanova_glmer"
+  class(mout) <- c("stanova", class(mout))
+  return(mout)
 }
 
 
