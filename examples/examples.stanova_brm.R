a6d55dc462a7093ee3ccc21b379078c275e97020
Henrik Singmann <singmann@gmail.com>
2020-11-22 15:28:20 +0100

    added BGLMM brms example

--- examples/examples.stanova_brm.R
+++ examples/examples.stanova_brm.R
@@ -4,3 +4,28 @@ fit_warp_brms <- stanova_brm(breaks ~ wool * tension, data = warpbreaks,
                              chains = 2, iter = 1000)
 summary(fit_warp_brms)
 }
+
+\dontrun{
+### binomial GLMM
+
+data("stroop", package = "afex")
+## for frequentist analysis see:
+### https://cran.r-project.org/package=afex/vignettes/afex_analysing_accuracy_data.html
+library("tidyverse")
+stroop_e1 <- stroop %>%
+  filter(!is.na(acc)) %>%
+  filter(study == "1") %>%
+  droplevels() %>%
+  group_by(condition, congruency, pno) %>%
+  summarise(
+    succ = sum(acc),
+    acc = mean(acc),
+    n = n())
+
+bglmm_stroop <- stanova_brm(
+  succ | trials(n) ~ congruency*condition + (congruency*condition|pno),
+  data = stroop_e1, family = binomial,
+  chains = 2, iter = 1000
+)
+summary(bglmm_stroop)
+}
